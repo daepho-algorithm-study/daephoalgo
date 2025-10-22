e1e161ad11dde575d006c89e35b19c5dc597113f
이용재 <lyj3023@naver.com>
2025-10-22 20:19:04 +0900

    2025.10_3 week Algorithm

---
+++ 용재/algorithm/src/baekjoon/Bronze/level3/p24265.java
@@ -0,0 +1,14 @@
+package baekjoon.Bronze.level3;
+
+import java.io.*;
+
+public class p24265 { // 백준 24265번 문제, 알고리즘 수업 - 알고리즘의 수행시간 4 (시간 복잡도)
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long n = Integer.parseInt(br.readLine());
+
+        System.out.println((n*(n - 1) / 2));
+        System.out.println(2);
+    }
+}
