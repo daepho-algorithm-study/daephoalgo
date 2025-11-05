bca39d012dc7cd05ca14c999dd6b9f7a61453495
이용재 <lyj3023@naver.com>
2025-11-05 16:55:24 +0900

    2025.11-1st Algorithm

---
+++ 용재/algorithm/src/baekjoon/Bronze/level2/p2750.java
@@ -0,0 +1,21 @@
+package baekjoon.Bronze.level2;
+
+import java.util.*;
+import java.io.*;
+
+public class p2750 { // 백준 2750번 문제, 수 정렬하기 (정렬)
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        for (int j = 0; j < n; j++) {
+            System.out.println(arr[j]);
+        }
+    }
+}
