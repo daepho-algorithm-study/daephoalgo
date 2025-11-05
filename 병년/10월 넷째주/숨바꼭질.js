2d4f44f4d4613b7a959d12d723c97d6f2db83775
kimbyeongnyeon <qud5252@naver.com>
2025-11-05 18:20:54 +0900

    refactor: 변수명 변경

--- 병년/10월 넷째주/숨바꼭질.js
+++ 병년/10월 넷째주/숨바꼭질.js
@@ -11,26 +11,26 @@ const input = fs
   .split("\n");
 
 const solution = (input) => {
-    const [N, K] = input.split(" ").map(Number);
-    const visited = Array.from({ length: 100100 }, () => false);
+  const [N, K] = input.split(" ").map(Number);
+  const visited = Array.from({ length: 100100 }, () => false);
 
-    const bfs = () => {
-        let queue = [[N, 0]]
-        visited[N] = true
-        while(queue.length > 0) {
-            let [cur, time] = queue.shift()
+  const bfs = () => {
+    let queue = [[N, 0]];
+    visited[N] = true;
+    while (queue.length > 0) {
+      let [cur, time] = queue.shift();
 
-            if (cur === K) return time
-            let next = [cur - 1, cur + 1, cur * 2]
-            for (let i = 0; i < 3; i++) {
-                if (!visited[next[i]] && 0 <= next[i] && next[i] < 1000000) {
-                    queue.push([next[i], time + 1])
-                    visited[next[i]] = true
-                }
-            }
+      if (cur === K) return time;
+      let next = [cur + 1, cur - 1, cur * 2];
+      for (let i = 0; i < 3; i++) {
+        if (!visited[next[i]] && 0 <= next[i] && next[i] < 1000000) {
+          queue.push([next[i], time + 1]);
+          visited[next[i]] = true;
         }
+      }
     }
-    console.log(bfs())
-}
+  };
+  console.log(bfs());
+};
 
-solution(input[0])
+solution(input[0]);
