ef9164d2e9461816178953ed4033daa91faedfa9
KimSeHyeon <61528451+kim13245@users.noreply.github.com>
2025-11-20 00:03:51 +0900

    백준: 좌표 정렬하기

---
+++ 세현/좌표 정렬하기.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n")
+//   .map((e) => Number(e));
+  .map((line) => line.split(" ").map(Number));
+
+// console.log(input);
+
+function solution(input) {
+  const arr = input.slice(1);
+  arr.sort((a, b) => {
+    if (a[0] === b[0]) {
+        return a[1] - b[1];
+    } else {
+        return a[0] - b[0];
+    }
+  });
+
+  return arr.map(row => row.join(" "));
+}
+
+console.log(solution(input).join('\n'));
