cea26b89a4770bd8db489c0ddd83eaa2bd053beb
이용재 <lyj3023@naver.com>
2025-12-03 20:25:55 +0900

    12월 1주차 알고리즘 풀이

---
+++ 용재/algorithm/src/baekjoon/Silver/level4/p2164.java
@@ -0,0 +1,22 @@
+package baekjoon.Silver.level4;
+
+import java.io.*;
+import java.util.*;
+
+public class p2164 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            q.add(i);
+        }
+        for (int j = 0; j < N-1; j++) {
+            q.poll();
+            q.add(q.poll());
+        }
+        System.out.println(q.poll());
+    }
+}
