8b21e13157f83bc6bf60f09eac219d21084bbc7c
KimSeHyeon <61528451+kim13245@users.noreply.github.com>
2026-03-18 22:20:07 +0900

    주석 추가

--- 세현/랜선 자르기.js
+++ 세현/랜선 자르기.js
@@ -9,19 +9,30 @@ const input = require("fs")
 
 function solution(N, K, lans) {
   let answer = 0;
-  // left = 0으로 하면 터짐
-  let [left, right] = [1, Math.max(...lans)]
 
+  // left = 0으로 하면 터짐. 1 으로 둘 것
+  let [left, right] = [1, Math.max(...lans)]
+  
+  // 범위가 존재할 때까지 탐색
   while (left <= right) {
+
+    // 중간 값 몫연산
     const middle = ~~((left + right) / 2)
+
+    // 현재 길이로 잘랐을 때 랜선 개수
     let total = 0
     for (let i = 0; i < K; i++) {
       total += ~~(lans[i] / middle);
     }
 
+    // 개수가 모자라면 길이가 더 작아야 함
     if (total < N) {
       right = middle - 1;
+
+    // 개수가 충분하면 길이가 더 커도 됨
     } else {
+
+      // 갈이 최대값으로 갱신
       answer = middle;
       left = middle + 1;
     }
