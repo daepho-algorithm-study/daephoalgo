29bd78cf643fffd3a322f3cffd6e1578b9e2c0e9
이용재 <lyj3023@naver.com>
2026-03-18 22:05:53 +0900

    2026.03.11~2026.03.18 알고리즘

---
+++ 용재/algorithm/src/baekjoon/Gold/level1/p3665.java
@@ -0,0 +1,10 @@
+package baekjoon.Gold.level1;
+
+import java.io.*;
+import java.util.*;
+
+public class p3665 { // 백준 3665번 문제, 최종 순위
+    public static void main(String[] args) throws IOException {
+
+    }
+}
