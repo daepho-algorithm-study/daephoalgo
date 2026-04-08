1b23e0d9bf01d294e4a232d16e3b2be6457e9069
kimbyeongnyeon <qud5252@naver.com>
2026-04-08 18:24:42 +0900

    feat: 4월 첫째주 문제 풀이

--- 병년/4월 첫째주/노란불 신호등.js
+++ 병년/4월 첫째주/노란불 신호등.js
@@ -1,8 +1,10 @@
 const solution = (signals) => {
   let shin = signals.length;
+
   const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
   const lcm = (a, b) => (a * b) / gcd(a, b);
   let len = signals.map(([g, y, r]) => g + y + r).reduce((a, b) => lcm(a, b));
+
   let count = Array(len).fill(0);
   for (let i = 0; i < signals.length; i++) {
     let green = signals[i][0];
@@ -12,9 +14,10 @@ const solution = (signals) => {
     let start = green + 1;
     let period = green + red + yello;
     let duration = yello;
-    for (let j = 0; j < len; j++) {
-      if (j >= start && (j - start) % period < duration) {
-        count[j]++;
+    for (let t = start; t < len; t += period) {
+      for (let d = 0; d < duration; d++) {
+        const idx = t + d;
+        if (idx < len) count[idx]++;
       }
     }
   }
